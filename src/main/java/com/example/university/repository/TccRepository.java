e215293acba7814481b5bd4bee9a49b042a68ccd
Emilly Wolski <emilly.wolski@gmail.com>
2025-02-07 23:25:27 -0300

    feat: :sparkles: Retirada de importação não utilizadas (TccController e TccRepository)

--- src/main/java/com/example/university/repository/TccRepository.java
+++ src/main/java/com/example/university/repository/TccRepository.java
@@ -1,13 +1,8 @@
 package com.example.university.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.university.model.Tcc;
 
 public interface TccRepository extends JpaRepository<Tcc, Long> {
-
-    // Optional<Tcc> findByCreatedBy(String userEmail);
 
 }
