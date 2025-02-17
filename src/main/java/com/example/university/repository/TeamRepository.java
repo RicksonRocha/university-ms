e0093237c48acefe22db08602c87e86b116851ef
Emilly Wolski <emilly.wolski@gmail.com>
2025-02-17 20:11:43 -0300

    feat: :sparkles: Exclusão de arquivos "Team" que não estão sendo utilizados. Estamos usando os arquivos de Tcc

--- src/main/java/com/example/university/repository/TeamRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.university.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.university.model.Team;
-
-public interface TeamRepository extends JpaRepository<Team, Long> {
-
-}
